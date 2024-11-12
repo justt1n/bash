e0225497f0e7faf39fd8402f14d1336f450100f6
tinnt <tinnt@airtrip-intl.com>
2024-11-12 10:11:07 +0700

    edit config

--- SetPersonalGit.sh
+++ SetPersonalGit.sh
@@ -1,3 +1,18 @@
-git config --local user.email "tinnt0709@gmail.com"
-git config --local user.name "justt1n"
+#!/bin/bash
+
+DEFAULT_EMAIL="tinnt0709@gmail.com"
+DEFAULT_USERNAME="justt1n"
+
+if [ "$#" -eq 2 ]; then
+  EMAIL=$1
+  USERNAME=$2
+else
+  EMAIL=$DEFAULT_EMAIL
+  USERNAME=$DEFAULT_USERNAME
+fi
+
+git config --local user.email "$EMAIL"
+git config --local user.name "$USERNAME"
 git config --local core.editor "vim"
+
+echo "Git configuration set with email: $EMAIL and username: $USERNAME"
